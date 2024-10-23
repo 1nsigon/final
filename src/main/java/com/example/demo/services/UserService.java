45d0dd7fe671fb7c136be7f16cc8423061438f3a
1nsigon <Ellie795@gmail.com>
2024-10-22 23:56:53 -0500

    Jwt Authentication Spring Security

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,9 +1,6 @@
 package com.example.demo.services;
 import com.example.demo.models.Role;
 import com.example.demo.models.User;
-import com.example.demo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.List;
 
 public interface UserService
